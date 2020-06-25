80b405cd6a9240dcdf26df844213eacf230066ad
Lode Claassen <lode@alsvanzelf.nl>
2020-06-25 19:09:20 +0200

    test

---
+++ test.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * cupcake ipsum:
+ * 
+ * Icing halvah toffee topping tootsie roll powder. Chocolate bar macaroon powder sweet marshmallow chocolate bar bear claw macaroon chocolate. Oat cake sweet roll topping halvah topping chocolate bar bonbon brownie bear claw. Gingerbread macaroon jelly-o candy canes. Chocolate cake danish cake tootsie roll lemon drops liquorice gingerbread pie. Danish pie sugar plum tiramisu. Sesame snaps jelly jelly-o macaroon jelly. Lollipop muffin biscuit dragée. Lollipop pie jelly-o gingerbread wafer oat cake gingerbread marshmallow. Marzipan jelly lemon drops sweet roll apple pie. Jelly beans cookie pudding apple pie carrot cake jelly beans chocolate powder. Oat cake apple pie cotton candy oat cake muffin oat cake lollipop.
+ */
+
+$array = ['foo'];
+$array = array('bar');
+
+function baz() {
+	echo 'hello';
+  echo 'world';
+	echo "!";
+}
